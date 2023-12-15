135392fc50a80ce953d9811a3e879911d4ac0c5c
Faisal Omar <93327868+faom002@users.noreply.github.com>
2023-12-15 12:25:51 +0100

    Delete TDS200_h23_2089_2082_2065 directory

--- TDS200_h23_2089_2082_2065/types.ts
+++
@@ -1,8 +0,0 @@
-export type RootStackParamList = {
-  Home: undefined;
-  Detail: { photo: { id: string; caption: string; image: string } };
-  Upload: undefined;
-  TakePic: undefined;
-  TabBarView: undefined;
-  LoginScreen: undefined;
-};
